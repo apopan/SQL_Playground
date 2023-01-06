d620e2042d4e089b7eeb35d0bd53d94d4811bf88
Apoorv Pandit <82618041+apopan@users.noreply.github.com>
2023-01-06 16:25:59 +0530

    Create Hackerrank_SQL_prepare.sql

---
+++ Hackerrank_SQL_prepare.sql
@@ -0,0 +1,49 @@
+## Revising select query 1 
+
+SELECT *
+FROM CITY
+WHERE POPULATION > 100000 AND COUNTRYCODE = "USA";
+
+
+## Revising select query 2
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = "USA" AND POPULATION > 120000;
+
+
+## Select all
+
+SELECT * FROM City;
+
+## Select by ID
+
+SELECT * FROM City WHERE ID=1661;
+
+
+## Japanese Cities' Attributes
+
+SELECT * FROM City WHERE CountryCode="JPN";
+
+
+## Japanese Cities' Names
+
+SELECT Name
+FROM City
+WHERE CountryCode = "JPN";
+
+
+## Weather Observations table 1 
+
+SELECT CITY, STATE
+FROM STATION
+ORDER BY CITY, STATE;
+
+
+## Weather Observations table 3
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (ID % 2) = 0
+ORDER BY CITY;
+
