ca61c67f91a331a418619b079c302208a31b0264
Apoorv Pandit <82618041+apopan@users.noreply.github.com>
2023-02-10 23:49:17 +0530

    Update Hackerrank_SQL_prepare.sql

--- Hackerrank_SQL_prepare.sql
+++ Hackerrank_SQL_prepare.sql
@@ -99,3 +99,5 @@ WHERE  city NOT REGEXP '^[aeiouAEIOU].*[aeiouAEIOU]$'
 select distinct city
 from station 
 where not (city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or  city like 'U%') and not (city like '%a' or city like '%e' or city like '%i' or city like '%o' or  city like '%u');
+
+select NAME from STUDENTS where MARKS > 75 order by RIGHT(NAME,3) asc,ID asc;
